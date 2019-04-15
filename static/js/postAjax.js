92fad86836e1800b8e1d5b11df3464ebcf4d39c9
Delta Castro <abel.castro9111@gmail.com>
2019-04-15 21:30:03 +0000

    Add postAjax funxtion

--- static/js/postAjax.js
+++ static/js/postAjax.js
@@ -1,22 +1,25 @@
 // Args... url, mehtod, body, callback, headers
 function deltaAjax (url, method, body, headers = null, callback) {
 
+  let token = document.querySelector('meta[name="csrfmiddlewaretoken"]').getAttribute('content')
+  body.append('csrfmiddlewaretoken', token)
   let headerDefault = {headers: {
     'Content-type': 'application/json'
   }}
   headers = headers || headerDefault
-  console.log(callback)
-   
-  // fetch(url, {
-  //   headers,
-  //   method: method,
-  //   body: body
-  // }).then(function (data) {
-  //   console.log(data)
-  //   callback()
-  // })
+  fetch(url, {
+    headers,
+    method: method,
+    body: body
+  }).then(function (response) {
+    console.log(response)
+    return response.json()
+  }).then(function (data) {
+    callback(data)
+  })
 }
 
 function test (msg) {
   console.log('este es tu mensaje je je', msg)
 }
+
